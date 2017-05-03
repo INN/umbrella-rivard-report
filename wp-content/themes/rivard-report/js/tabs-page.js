8a30698a7819b857a2645f607aff470ce868e467
Julia Smith <julia@inn.org>
2017-05-02 19:17:38 -0500

    styles for donation page

--- wp-content/themes/rivard-report/js/tabs-page.js
+++ wp-content/themes/rivard-report/js/tabs-page.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 	$('#tabs').tabs();
 
-	//hover states on the static widgets
+	// hover states on the static widgets
 	$('#dialog_link, ul#icons li').hover(
 	function() { $(this).addClass('ui-state-hover'); },
 	function() { $(this).removeClass('ui-state-hover'); }
@@ -14,5 +14,21 @@ jQuery(document).ready(function($) {
 		header: 'h4',
 		heightStyle: "content",
 	});
+
+	// for donation page
+	if ($('body').hasClass('page-id-184947')){
+		$('.rivard-donation-form').hide();
+		var selected = $('input[name="membership-type"]:checked').val();
+		console.log(selected);
+
+		$('.rivard-donation-form.level-' + selected).show();
+
+		var $input = $('input[name="membership-type"]');
+		$input.on('click', function(e) {
+			selected = $('input[name="membership-type"]:checked').val();
+			$('.rivard-donation-form').hide();
+			$('.rivard-donation-form.level-' + selected).show();
+		});
+	}
 
 });
